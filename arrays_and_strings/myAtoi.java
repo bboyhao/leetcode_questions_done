0746f041772083481852ff603ef65437497bf394
bboyhao <wuhao241295@gmail.com>
2018-11-14 12:14:15 +0800

    Better solution for myAtoi

--- arrays_and_strings/myAtoi.java
+++ arrays_and_strings/myAtoi.java
@@ -1,35 +1,26 @@
 class Solution {
     public int myAtoi(String str) {
-        str=str.trim();
-        if(str.length()==0) return 0;
-        if(str.length()==1&&!Character.isDigit(str.charAt(0))) return 0;
-        if(!Character.isDigit(str.charAt(0))&&str.charAt(0)!='+'&&str.charAt(0)!='-') return 0;
-        if(!Character.isDigit(str.charAt(0))&&!Character.isDigit(str.charAt(1))) return 0;
-        int length = str.length();
-        int i = 0;
-        long result=0;
-        boolean negative=false;
-        boolean overflow=false;
-        while(i<length){
-            char c = str.charAt(i);
-            if(i==0){
-                if(c=='+'){i+=1;continue;}
-                if(c=='-'){i+=1;negative = true;continue;}
-            }
-            if(!Character.isDigit(c)) break;
-            if(result>result*10+Character.getNumericValue(c)){overflow=true;break;}            
-            result=result*10+Character.getNumericValue(c);
-            i+=1;
+      if(str==null) return 0;
+
+      str = str.trim();
+      if(str.length()==0) return 0;
+      char[] array=str.toCharArray();
+      int sign = 1;
+      int i = 0;
+      if(array[i]=='+'){sign =1;i+=1;}
+      else if (array[i]=='-'){sign=-1;i+=1;}
+
+      int result=0;
+      while(i<array.length){
+        int digit = array[i]-'0';
+        if(digit<0||digit>9){
+          return result*sign;
         }
-        if(overflow){
-            if(negative) return Integer.MIN_VALUE;
-            return Integer.MAX_VALUE;
-        }
-        System.out.println(result);
-        if(negative) result=0-result;
-        if(result>Integer.MAX_VALUE)return Integer.MAX_VALUE;
-        if(result<Integer.MIN_VALUE)return Integer.MIN_VALUE;
-        return (int)result;
-        
+        if(result>Integer.MAX_VALUE/10||(result==Integer.MAX_VALUE/10&&digit>Integer.MAX_VALUE%10))
+          return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        result = result*10+digit;
+        i+=1;
+      }
+      return result*sign;
     }
 }
