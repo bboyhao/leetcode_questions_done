4511ae614842733defa0f0be59c7c26a0eb67e91
bboyhao <wuhao241295@gmail.com>
2024-06-21 19:49:03 +0800

    add 56.Merge Intervals

--- arrays_and_strings/MergeIntervals.java
+++ arrays_and_strings/MergeIntervals.java
@@ -1,28 +1,59 @@
-public class MergeIntervals{
-    private class IntervalComparator implements Comparator<Interval> {
-        @Override
-        public int compare(Interval a, Interval b) {
-            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+import java.util.*;
+
+class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals==null || intervals.length == 1) return intervals;
+        sort(intervals);
+        int cur = 1;
+        int last = 0;
+        while(cur < intervals.length) {
+            if(overlap(intervals[last], intervals[cur])) {
+                int[] merged = mergeInterval(intervals[last], intervals[cur]);
+                intervals[last] = merged;
+            } else{
+                intervals[++last] = intervals[cur];
+            }
+            cur++;
         }
+        return Arrays.copyOf(intervals, last+1);
     }
 
-    public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new IntervalComparator());
+    private static boolean overlap(int[] a, int[] b){
+        if (Math.max(a[0], b[0])<=Math.min(a[1], b[1])) return true;
+        return false;
+    }
 
-        LinkedList<Interval> merged = new LinkedList<Interval>();
-        for (Interval interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.isEmpty() || merged.getLast().end < interval.start) {
-                merged.add(interval);
-            }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+    private int[] mergeInterval(int[] a, int[] b) {
+        return new int[]{Math.min(a[0],b[0]), Math.max(a[1], b[1])};
+    }
+
+    private void sort(int[][] intervals) {
+        if(intervals == null || intervals.length <=1) return;
+        int size = intervals.length;
+        quicksort(intervals, 0, size-1);
+    }
+
+    private void quicksort(int[][] intervals, int left, int right){
+        if (left >= right) return;
+        int pivot = partition(intervals, left, right);
+        quicksort(intervals, left, pivot-1);
+        quicksort(intervals, pivot+1, right);
+    }
+
+    private int partition(int[][] intervals, int left, int right) {
+        int lastsmall = left;
+        for(int i = left+1;i<=right;i++) {
+            if(intervals[i][0]<intervals[left][0]) {
+                swap(intervals, i, ++lastsmall);
             }
         }
+        swap(intervals, left, lastsmall);
+        return lastsmall;
+    }
 
-        return merged;
+    private void swap(int[][] intervals, int i, int j) {
+        int[] tmp = intervals[i];
+        intervals[i] = intervals[j];
+        intervals[j] = tmp;
     }
 }
